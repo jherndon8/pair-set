f146ebe0964f1a6ec214fec0449711dc7924e31d
jherndon8 <jherndon8@gatech.edu>
2023-09-15 17:48:39 -0400

    first push point

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express")
+var bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json())
 let port = process.env.PORT || 3000
 
+const scores = []
 app.get("/", (req, res) => {
     res.send("hello world")
-})
+});
+
+app.post("/addScore", (req, res) => {
+    console.log(req.body)
+    scores.push(req.body.score)
+    res.json({result :scores})
+});
 
 
 app.listen(port, () => {
